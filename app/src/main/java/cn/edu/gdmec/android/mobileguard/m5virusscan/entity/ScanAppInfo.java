b9b5829d2028e095317546563ea8cdf0a4738f3d
wzf383 <383225639@qq.com>
2017-11-18 11:13:39 +0800

    病毒测试3关3

--- app/src/main/java/cn/edu/gdmec/android/mobileguard/m5virusscan/entity/ScanAppInfo.java
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m5virusscan/entity/ScanAppInfo.java
@@ -3,13 +3,13 @@ package cn.edu.gdmec.android.mobileguard.m5virusscan.entity;
 import android.graphics.drawable.Drawable;
 
 /**
- * Created by 38322 on 2017/11/15.
+ * Created by Administrator on 2017/11/13.
  */
 
 public class ScanAppInfo {
     public String appName;
-    public  boolean isVirus;
+    public boolean isVirus;
     public String packagename;
-    public  String description;
+    public String description;
     public Drawable appicon;
 }
